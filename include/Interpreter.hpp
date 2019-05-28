4fbe7df5cb5d0fbf9c2faa5cb69d6cc792c27f08
Thordreck <paezguerraalvaro@gmail.com>
2019-05-28 22:13:23 +0200

    Initialize interpreter memory with default sprites. Renamed utils folder to details.

--- include/Interpreter.hpp
+++ include/Interpreter.hpp
@@ -23,8 +23,8 @@ namespace chip8
             memory::Ram ram_;
             memory::RamIter program_counter_;
 
-            const memory::RamIter program_memory_       { ram_.begin() + memory::begin_program_ram };
-            const memory::RamIter end_interpter_memory_ { ram_.begin() + memory::interpreter_ram_size };
+            const memory::RamIter program_memory_         { ram_.begin() + memory::begin_program_ram };
+            const memory::RamIter end_interpreter_memory_ { ram_.begin() + memory::interpreter_ram_size };
     };
 }
 #endif
