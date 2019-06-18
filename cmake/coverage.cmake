39330d7c9f1975b67db169fc9a0823a32578a224
WooWapDaBug <blascoburguillos@gmail.com>
2019-06-18 13:13:51 +0200

    wip

--- cmake/coverage.cmake
+++
@@ -1,5 +0,0 @@
-macro(target_add_coverage TARGET_NAME_ARG)
-    set(COVERAGE_EXCLUDES "/usr/include/\\*;")
-    target_compile_options(${TARGET_NAME_ARG} PRIVATE --coverage)
-    target_link_libraries(${TARGET_NAME_ARG} PRIVATE --coverage)
-endmacro(target_add_coverage)
